b800f95c9bb0e2d8f7e92362be73b9c07ec4ffc1
Caleb Ellis <calebellis2805@gmail.com>
2017-08-03 07:38:06 +0100

    changed contact info + disabled autoscroll on mobile

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ $('a.scroll').click(function(){
 });
 
 $('#top').on('mousewheel', function(e) {
-  if (allowAutoScroll === true && e.deltaY <= 0) {
+  if (allowAutoScroll === true && e.deltaY <= 0 && window.innerWidth > 839 ) {
     allowAutoScroll = false;
     scrollTo('#aboutMe');
   }
