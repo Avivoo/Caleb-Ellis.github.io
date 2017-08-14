bb5b03c527e2031dddb201984573103469d93e67
Caleb Ellis <calebellis2805@gmail.com>
2017-08-14 16:48:29 +0100

    updated resume + removed carousel

--- js/index.js
+++ js/index.js
@@ -26,26 +26,3 @@ $(window).on('load', function() {
     distance: '10vh'
   }, 100);
 });
-
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-    loop: false,
-    responsiveClass: true,
-    nav: true,
-    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    responsive: {
-      0: {
-        items: 1,
-        slideBy: 1
-      },
-      480: {
-        items: 2,
-        slideBy: 2
-      },
-      840: {
-        items: 3,
-        slideBy: 3
-      }
-    }
-  });
-});
